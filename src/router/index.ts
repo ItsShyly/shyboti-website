624d24377b4d8cd0e98241965f476ebae7e842f9
Shyly <shyly>
2026-03-12 13:31:14 +0100

    added home endpoint to Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',            redirect: '/dashboard' },
+    { path: '/',            redirect: '/home' },
+    { path: '/home',        component: () => import('../components/HomeView.vue') },
     { path: '/dashboard',   component: () => import('../components/DashboardView.vue') },
     { path: '/commands',    component: () => import('../components/CommandsView.vue') },
     { path: '/logs',        component: () => import('../components/LogsView.vue') },
