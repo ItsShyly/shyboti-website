7d57cea7f73fae35c827e740f960bf8c6839942b
Shyly <shyly>
2026-04-18 08:39:59 +0200

    increase buffer size

--- src/components/LogsView.vue
+++ src/components/LogsView.vue
@@ -1835,7 +1835,7 @@ function paintNameStyle(paint: { imageUrl: string | null; stops: { at: number; c
           :items="displayItems"
           :min-item-size="28"
           key-field="id"
-          :buffer="1200"
+          :buffer="4000"
           @update="onScrollerUpdate"
         >
           <template #before>
