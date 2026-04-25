5748978a0a5e0c3f3484a539f35f42696b8e7770
Shyly <shyly>
2026-04-25 18:13:46 +0200

    remove not logged in button

--- src/App.vue
+++ src/App.vue
@@ -309,7 +309,6 @@ provide('searchOpenTrigger', searchOpenTrigger)
           <button class="sidebar-logout" @click="logout(); router.push('/'); sidebarOpen = false">{{ t('nav.logout') }}</button>
         </template>
         <template v-else>
-          <span class="sidebar-user" style="color:#555">Not logged in</span>
           <button class="sidebar-login-btn" :class="{ shake: loginShaking }" @click="login; sidebarOpen = false">{{ t('nav.login') }}</button>
         </template>
       </div>
